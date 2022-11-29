1a062265e56682d2bec6047492540fc108f64168
Tarek994 <ghali.tarek.123@hotmail.com>
2022-11-29 10:26:41 +0200

    orgnize(services): organise the new services section

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -5,6 +5,14 @@ const Services = () => {
         <div className="services-title">
             <h1>Services</h1>
         </div>
+
+        <div className="services-items">
+          <img className="services-image" src="" alt="" />
+        </div>
+
+        <div className="services-description">
+          
+        </div>
     </div>
   )
 }
