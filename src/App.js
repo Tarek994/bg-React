1ba57f3f41c6727e9e036e724381efbeea19e625
Tarek994 <ghali.tarek.123@hotmail.com>
2022-12-02 17:03:54 +0200

    chore(app): import contact component to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Main from "./Components/Main"
 import Services from "./Components/Services";
+import Contact from "./Components/Contact";
 
 function App() {
   return (
     <div className="App">
       <Main />
       <Services/>
+      <Contact/>
     </div>
   );
 }
