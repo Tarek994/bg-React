a2048cc7ef586c43b1742f1ed4a5c5956b4f0783
Tarek994 <ghali.tarek.123@hotmail.com>
2022-12-03 22:02:23 +0200

    content(contact): add the contact elements

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,10 +1,27 @@
+import old from "../assets/old.webp"
+import newimg from "../assets/newimg.webp"
 
 const Contact = () => {
   return (
+    <>
     <div className="contact">
-        Contact
-        
+        <div className="contact-left">
+        <div className="contact-title">
+            <h1 className="">Contact us</h1>
+        </div>
+          <div >
+            <img className="contact-image" src={old} alt=""/>
+          </div>
+          
+          
+        </div>
+
+        <div className="contact-right">
+
+        </div>
+
     </div>
+    </>
   )
 }
 
