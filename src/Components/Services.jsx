fc726bc4547947b2665bfb9e4b067e327bc3abd2
Tarek994 <ghali.tarek.123@hotmail.com>
2022-12-03 21:46:30 +0200

    delete(services): delete the Trip code from Services component

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -1,5 +1,6 @@
 import newimg from "../assets/newimg.webp"
-import damas from "../assets/damas.jpeg"
+
+
 const Services = () => {
   return (
     <>
@@ -25,32 +26,10 @@ const Services = () => {
           </div>
         </div>
 
-        
-    </div>
-
-    <div className="trip">
-        <div className="trip-title">
-                <h1> Our Trips</h1>
-        </div>
-
-        <div className="trip-items">
-        <img className="trip-image" src={damas} alt="" />
-        </div>
-
-        <div className="trip-description">
-            <ul className="trip-list">
-              <li> Asperiores repudiandae at modi velit laborum odit</li>
-              <li> Visit to the most beautiful places ever</li>
-              <li> There are numerous locations that speak to their heritage. </li>
-              <li> We roam your world in search of luxury</li>
-            </ul>
-            <button className="trip-btn">
-            Explore the culture !
-            </button>
-            
-          </div>
 
     </div>
+
+
 
     </>
   )
