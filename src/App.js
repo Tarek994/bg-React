22673acaa9cba9c3cb4726f701e6596b0d8c4769
Tarek994 <ghali.tarek.123@hotmail.com>
2022-12-04 15:16:10 +0200

    call(App): call the Trip and Footer components into the App

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Main from "./Components/Main"
 import Services from "./Components/Services";
 import Contact from "./Components/Contact";
-
+import Trip from "./Components/Trip";
+import Footer from "./Components/Footer";
 function App() {
   return (
     <div className="App">
       <Main />
       <Services/>
+      <Trip/>
       <Contact/>
     </div>
   );
