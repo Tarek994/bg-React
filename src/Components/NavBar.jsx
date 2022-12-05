d0abbb686e1fc86ef529706171c97a5c6afcbdb9
Tarek994 <ghali.tarek.123@hotmail.com>
2022-12-06 01:49:24 +0200

    buttons(navbar): Add the Navbar buttons bar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,11 +1,20 @@
 
+
 const NavBar = () => {
   return (
-    <div className="navbar">
-        <a href="/" className="logo">Super Trip</a>
-        
+    <div className="main-nav">
+        <div className="navbar">
+            <a href="/" className="logo"> Super Trip </a>
+
+            <div className="navigation">
+                <a href="#services">SERVICES</a>
+                <a href="#projects">TRIPS</a>
+                <a href="#contact">CONTACT</a>
+            </div>
+            
+        </div>
     </div>
   )
 }
 
-export default NavBar
+export default NavBar;
