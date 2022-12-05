ae96399d37345acbdd67f2bbdba322549ea2060e
Tarek994 <ghali.tarek.123@hotmail.com>
2022-12-05 23:57:49 +0200

    import(App): import the Navbar component into App

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import Services from "./Components/Services";
 import Contact from "./Components/Contact";
 import Trip from "./Components/Trip";
 import Footer from "./Components/Footer";
+import NavBar from "./Components/NavBar";
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <Main />
       <Services/>
       <Trip/>
