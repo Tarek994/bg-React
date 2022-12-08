ca3d0df4c955450ed242034e67fa5a30ed2efbf0
Tarek994 <ghali.tarek.123@hotmail.com>
2022-12-08 16:58:46 +0200

    chore(Trip): remove unuse code and white space

--- src/Components/trip/Trip.jsx
+++ src/Components/trip/Trip.jsx
@@ -3,7 +3,7 @@ import damas from "../../assets/damas.jpeg"
 
 const Trip = () => {
   return (
-    <>
+    
     <div className="trip">
         <div className="trip-title">
                 <h1> Our Trips</h1>
@@ -17,7 +17,7 @@ const Trip = () => {
             <ul className="trip-list">
               <li> Asperiores repudiandae at modi velit laborum odit</li>
               <li> Visit to the most beautiful places ever</li>
-              <li> There are numerous locations that speak to their heritage. </li>
+              <li> There are numerous locations that speak to their heritage.</li>
               <li> We roam your world in search of luxury</li>
             </ul>
             <a className="trip-btn" href="https://www.google.com/travel/flights">
@@ -27,7 +27,7 @@ const Trip = () => {
           </div>
 
     </div>
-    </>
+    
   )
 }
 
