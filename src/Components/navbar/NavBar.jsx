2cfc7eeb9ddcceee53268997d25135f5d786ccf2
Tarek994 <ghali.tarek.123@hotmail.com>
2022-12-30 23:25:40 +0200

    style(navbar): set up the navbar and the style for it

--- src/Components/navbar/NavBar.jsx
+++ src/Components/navbar/NavBar.jsx
@@ -2,14 +2,14 @@ import "./Navbar.css"
 
 const Navbar = () => {
   return (
-    <div className="main-nav">
-        <div className="navbar">
+    <div className="main-nav col-12">
+        <div className="navbar col-lg-8 m-auto ">
             <a href="/" className="logo"> Super Trip </a>
 
             <div className="navigation">
-                <a href="#services">SERVICES</a>
-                <a href="#projects">TRIPS</a>
-                <a href="#contact">CONTACT</a>
+                <a href="#services">SERVICES|</a>
+                <a href="#projects">TRIPS|</a>
+                <a href="#contact">CONTACT|</a>
             </div>
             
         </div>
