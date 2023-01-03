aa2610df95eab09afe20b2cc852fa29e15d0a568
Tarek994 <ghali.tarek.123@hotmail.com>
2023-01-03 23:56:33 +0200

    chore(app): uncommet the Trip component to build it

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Main from "./Components/main/Main"
 import Services from "./Components/services/Services";
 // import Contact from "./Components/contact/Contact";
-// import Trip from "./Components/trip/Trip";
+import Trip from "./Components/trip/Trip";
 // import Footer from "./Components/footer/Footer";
 import Navbar from "./Components/navbar/NavBar";
 
@@ -11,9 +11,9 @@ function App() {
     <div className="app">
         <Navbar/>
          {/* <Main/> */}
-       <Services/>
-         {/*<Trip/>
-        <Contact/>
+       {/* <Services/> */}
+         <Trip/>
+        {/*<Contact/>
         <Footer/> */}
     </div>
   );
