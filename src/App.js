ab9f6b6248d0aa6f0d3e3d66f2d93589e33bfc97
Tarek994 <ghali.tarek.123@hotmail.com>
2023-01-14 23:31:12 +0200

    chore(app): uncomment the components to test them

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from "react";
-// import Main from "./Components/main/Main"
-// import Services from "./Components/services/Services";
+import Main from "./Components/main/Main"
+import Services from "./Components/services/Services";
 import Contact from "./Components/contact/Contact";
-// import Trip from "./Components/trip/Trip";
-// import Footer from "./Components/footer/Footer";
+import Trip from "./Components/trip/Trip";
+import Footer from "./Components/footer/Footer";
 import Navbar from "./Components/navbar/NavBar";
 
 function App() {
   return (
     <div className="app">
-        {/* <Navbar/> */}
-         {/* <Main/>  */}
-       {/* <Services/> */}
-         {/* <Trip/>  */}
+        <Navbar/>
+         <Main/> 
+       <Services/>
+         <Trip/> 
         <Contact/>
-        {/* <Footer/>  */}
+        <Footer/> 
     </div>
   );
 }
